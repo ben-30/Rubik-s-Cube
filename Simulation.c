858a1ee840b0166522c5f0a0030c767201131282
Benjamin Nguyen <90567404+ben-30@users.noreply.github.com>
2022-02-09 01:01:20 -0500

    fix memory issues

--- Simulation.c
+++ Simulation.c
@@ -6,6 +6,7 @@
 void display();
 void reset();
 void perform_movement(char[]);
+void make_copy();
 void U();
 void D();
 void L();
@@ -14,15 +15,10 @@ void F();
 void B();
 
 char cube[54];
+char cube_copy[54];
 
 int main() {	
 	reset();
-	//U();
-	//D();
-	//L();
-	//R();
-	//F();
-	//B();
 	
 	char input_movement[100];
 	printf("Enter Movement String: ");
@@ -134,176 +130,177 @@ void perform_movement(char input_movement[])
 	}
 }
 
+void make_copy()
+{
+	for (int i=0; i<54; i++) {
+		cube_copy[i] = cube[i];
+	}
+}
+
 void U()
 {
-	char temp[54];
-	memcpy(temp, cube, sizeof cube);
+	make_copy();
 	
-	cube[9]  = temp[18];
-	cube[10] = temp[19];
-	cube[11] = temp[20];
-	cube[18] = temp[27];
-	cube[19] = temp[28];
-	cube[20] = temp[29];
-	cube[27] = temp[36];
-	cube[28] = temp[37];
-	cube[29] = temp[38];
-	cube[36] = temp[9] ;
-	cube[37] = temp[10];
-	cube[38] = temp[11];
+	cube[9]  = cube_copy[18];
+	cube[10] = cube_copy[19];
+	cube[11] = cube_copy[20];
+	cube[18] = cube_copy[27];
+	cube[19] = cube_copy[28];
+	cube[20] = cube_copy[29];
+	cube[27] = cube_copy[36];
+	cube[28] = cube_copy[37];
+	cube[29] = cube_copy[38];
+	cube[36] = cube_copy[9] ;
+	cube[37] = cube_copy[10];
+	cube[38] = cube_copy[11];
 	
-	cube[0]  = temp[6] ;
-	cube[1]  = temp[3] ;
-	cube[2]  = temp[0] ;
-	cube[3]  = temp[7] ;
-	cube[4]  = temp[4] ;
-	cube[5]  = temp[1] ;
-	cube[6]  = temp[8] ;
-	cube[7]  = temp[5] ;
-	cube[8]  = temp[2] ;
+	cube[0]  = cube_copy[6] ;
+	cube[1]  = cube_copy[3] ;
+	cube[2]  = cube_copy[0] ;
+	cube[3]  = cube_copy[7] ;
+	cube[4]  = cube_copy[4] ;
+	cube[5]  = cube_copy[1] ;
+	cube[6]  = cube_copy[8] ;
+	cube[7]  = cube_copy[5] ;
+	cube[8]  = cube_copy[2] ;
 }
 
 void D()
 {
-	char temp[54];
-	memcpy(temp, cube, sizeof cube);
+	make_copy();
 	
-	cube[15] = temp[42];
-	cube[16] = temp[43];
-	cube[17] = temp[44];
-	cube[24] = temp[15];
-	cube[25] = temp[16];
-	cube[26] = temp[17];
-	cube[33] = temp[24];
-	cube[34] = temp[25];
-	cube[35] = temp[26];
-	cube[42] = temp[33];
-	cube[43] = temp[34];
-	cube[44] = temp[35];
+	cube[15] = cube_copy[42];
+	cube[16] = cube_copy[43];
+	cube[17] = cube_copy[44];
+	cube[24] = cube_copy[15];
+	cube[25] = cube_copy[16];
+	cube[26] = cube_copy[17];
+	cube[33] = cube_copy[24];
+	cube[34] = cube_copy[25];
+	cube[35] = cube_copy[26];
+	cube[42] = cube_copy[33];
+	cube[43] = cube_copy[34];
+	cube[44] = cube_copy[35];
 	
-	cube[45] = temp[51];
-	cube[46] = temp[48];
-	cube[47] = temp[45];
-	cube[48] = temp[52];
-	cube[49] = temp[49];
-	cube[50] = temp[46];
-	cube[51] = temp[53];
-	cube[52] = temp[50];
-	cube[53] = temp[47];
+	cube[45] = cube_copy[51];
+	cube[46] = cube_copy[48];
+	cube[47] = cube_copy[45];
+	cube[48] = cube_copy[52];
+	cube[49] = cube_copy[49];
+	cube[50] = cube_copy[46];
+	cube[51] = cube_copy[53];
+	cube[52] = cube_copy[50];
+	cube[53] = cube_copy[47];
 }
 
 void L()
 {
-	char temp[54];
-	memcpy(temp, cube, sizeof cube);
+	make_copy();
 	
-	cube[0]  = temp[44];
-	cube[3]  = temp[41];
-	cube[6]  = temp[38];
-	cube[18] = temp[0] ;
-	cube[21] = temp[3] ;
-	cube[24] = temp[6] ;
-	cube[45] = temp[18];
-	cube[48] = temp[21];
-	cube[51] = temp[24];
-	cube[44] = temp[45];
-	cube[41] = temp[48];
-	cube[38] = temp[51];
+	cube[0]  = cube_copy[44];
+	cube[3]  = cube_copy[41];
+	cube[6]  = cube_copy[38];
+	cube[18] = cube_copy[0] ;
+	cube[21] = cube_copy[3] ;
+	cube[24] = cube_copy[6] ;
+	cube[45] = cube_copy[18];
+	cube[48] = cube_copy[21];
+	cube[51] = cube_copy[24];
+	cube[44] = cube_copy[45];
+	cube[41] = cube_copy[48];
+	cube[38] = cube_copy[51];
 	
-	cube[9]  = temp[15];
-	cube[10] = temp[12];
-	cube[11] = temp[9] ;
-	cube[12] = temp[16];
-	cube[13] = temp[13];
-	cube[14] = temp[10];
-	cube[15] = temp[17];
-	cube[16] = temp[14];
-	cube[17] = temp[11];
+	cube[9]  = cube_copy[15];
+	cube[10] = cube_copy[12];
+	cube[11] = cube_copy[9] ;
+	cube[12] = cube_copy[16];
+	cube[13] = cube_copy[13];
+	cube[14] = cube_copy[10];
+	cube[15] = cube_copy[17];
+	cube[16] = cube_copy[14];
+	cube[17] = cube_copy[11];
 }
 
 void R()
 {
-	char temp[54];
-	memcpy(temp, cube, sizeof cube);
+	make_copy();
 	
-	cube[2]  = temp[20];
-	cube[5]  = temp[23];
-	cube[8]  = temp[26];
-	cube[20] = temp[47];
-	cube[23] = temp[50];
-	cube[26] = temp[53];
-	cube[47] = temp[42];
-	cube[50] = temp[39];
-	cube[53] = temp[36];
-	cube[42] = temp[2] ;
-	cube[39] = temp[5] ;
-	cube[36] = temp[8] ;
+	cube[2]  = cube_copy[20];
+	cube[5]  = cube_copy[23];
+	cube[8]  = cube_copy[26];
+	cube[20] = cube_copy[47];
+	cube[23] = cube_copy[50];
+	cube[26] = cube_copy[53];
+	cube[47] = cube_copy[42];
+	cube[50] = cube_copy[39];
+	cube[53] = cube_copy[36];
+	cube[42] = cube_copy[2] ;
+	cube[39] = cube_copy[5] ;
+	cube[36] = cube_copy[8] ;
 	
-	cube[27] = temp[33];
-	cube[28] = temp[30];
-	cube[29] = temp[27];
-	cube[30] = temp[34];
-	cube[31] = temp[31];
-	cube[32] = temp[28];
-	cube[33] = temp[35];
-	cube[34] = temp[32];
-	cube[35] = temp[29];
+	cube[27] = cube_copy[33];
+	cube[28] = cube_copy[30];
+	cube[29] = cube_copy[27];
+	cube[30] = cube_copy[34];
+	cube[31] = cube_copy[31];
+	cube[32] = cube_copy[28];
+	cube[33] = cube_copy[35];
+	cube[34] = cube_copy[32];
+	cube[35] = cube_copy[29];
 }
 
 void F()
 {
-	char temp[54];
-	memcpy(temp, cube, sizeof cube);
+	make_copy();
 	
-	cube[6]  = temp[17];
-	cube[7]  = temp[14];
-	cube[8]  = temp[11];
-	cube[17] = temp[47];
-	cube[14] = temp[46];
-	cube[11] = temp[45];
-	cube[47] = temp[27];
-	cube[46] = temp[30];
-	cube[45] = temp[33];
-	cube[27] = temp[6] ;
-	cube[30] = temp[7] ;
-	cube[33] = temp[8] ;
+	cube[6]  = cube_copy[17];
+	cube[7]  = cube_copy[14];
+	cube[8]  = cube_copy[11];
+	cube[17] = cube_copy[47];
+	cube[14] = cube_copy[46];
+	cube[11] = cube_copy[45];
+	cube[47] = cube_copy[27];
+	cube[46] = cube_copy[30];
+	cube[45] = cube_copy[33];
+	cube[27] = cube_copy[6] ;
+	cube[30] = cube_copy[7] ;
+	cube[33] = cube_copy[8] ;
 	
-	cube[18] = temp[24];
-	cube[19] = temp[21];
-	cube[20] = temp[18];
-	cube[21] = temp[25];
-	cube[22] = temp[22];
-	cube[23] = temp[19];
-	cube[24] = temp[26];
-	cube[25] = temp[23];
-	cube[26] = temp[20];
+	cube[18] = cube_copy[24];
+	cube[19] = cube_copy[21];
+	cube[20] = cube_copy[18];
+	cube[21] = cube_copy[25];
+	cube[22] = cube_copy[22];
+	cube[23] = cube_copy[19];
+	cube[24] = cube_copy[26];
+	cube[25] = cube_copy[23];
+	cube[26] = cube_copy[20];
 }
 
 void B()
 {
-	char temp[54];
-	memcpy(temp, cube, sizeof cube);
+	make_copy();
 	
-	cube[0]  = temp[29];
-	cube[1]  = temp[32];
-	cube[2]  = temp[35];
-	cube[29] = temp[53];
-	cube[32] = temp[52];
-	cube[35] = temp[51];
-	cube[53] = temp[15];
-	cube[52] = temp[12];
-	cube[51] = temp[9] ;
-	cube[15] = temp[0] ;
-	cube[12] = temp[1] ;
-	cube[9]  = temp[2] ;
+	cube[0]  = cube_copy[29];
+	cube[1]  = cube_copy[32];
+	cube[2]  = cube_copy[35];
+	cube[29] = cube_copy[53];
+	cube[32] = cube_copy[52];
+	cube[35] = cube_copy[51];
+	cube[53] = cube_copy[15];
+	cube[52] = cube_copy[12];
+	cube[51] = cube_copy[9] ;
+	cube[15] = cube_copy[0] ;
+	cube[12] = cube_copy[1] ;
+	cube[9]  = cube_copy[2] ;
 	
-	cube[36] = temp[42];
-	cube[37] = temp[39];
-	cube[38] = temp[36];
-	cube[39] = temp[43];
-	cube[40] = temp[40];
-	cube[41] = temp[37];
-	cube[42] = temp[44];
-	cube[43] = temp[41];
-	cube[44] = temp[38];
+	cube[36] = cube_copy[42];
+	cube[37] = cube_copy[39];
+	cube[38] = cube_copy[36];
+	cube[39] = cube_copy[43];
+	cube[40] = cube_copy[40];
+	cube[41] = cube_copy[37];
+	cube[42] = cube_copy[44];
+	cube[43] = cube_copy[41];
+	cube[44] = cube_copy[38];
 }
