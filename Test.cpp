fe766dab08f559d08ac4b8e082b7d36ebb173ee3
Benjamin Nguyen <90567404+ben-30@users.noreply.github.com>
2022-04-04 14:55:10 -0400

    display reduced final_moves

--- Test.cpp
+++ Test.cpp
@@ -1,17 +1,31 @@
 #include "Algorithm.cpp"
 
 int main() {
-	std::string OWEN = "GYWBWGROYYRGYOWGYYYGOGGWRWWBROBROOORGROBBGWWRBRBBYYWOB";
+	//std::string OWEN = "YGYWWRWOYBOOWOYGGRGBBGGGYRWRWGRRBOOBOWOYBBRRRGYBOYYWBW";
+	std::string OWEN = "OBRWWORGBBGGWOWGBOYYWBGWYRRRYWOROBRGGRWBBRYOWBGYYYYOGO";
 	Algorithm a(OWEN);
+	a.solve();
 
-	printf("Final Count: %d", a.final_moves_count);
+	int original = a.final_moves_count;
+
+	printf("\nFinal Count: %d", a.final_moves_count);
 	printf("\nMoves to Solve: ");
 	std::string ERIC(a.final_moves);
-	for (int i=0; i<a.final_moves_count; i++) {
-		printf("%c", ERIC[i]);
-		if ((i+1)%10 == 0) { printf(" "); }
-		if ((i+1)%50 == 0) { printf("\n                "); }
-	}
+	for (int i=0; i<a.original_count; i++) { printf("%c", ERIC[i]); }
+	printf("\n");
+	
+	a.reverse_moves();
+	printf("\nRemove Reverse Moves! --> Final Count: %d", a.final_moves_count);
+	printf("\nMoves to Solve: ");
+	std::string ERIC_2(a.final_moves);
+	for (int i=0; i<a.original_count; i++) {	printf("%c", ERIC_2[i]); }
+	printf("\n");
+	
+	a.double_moves();
+	printf("\nRemove Double Moves! -->  Final Count: %d", a.final_moves_count);
+	printf("\nMoves to Solve: ");
+	std::string ERIC_3(a.final_moves);
+	for (int i=0; i<a.original_count; i++) {	printf("%c", ERIC_3[i]); }
 	printf("\n");
 
 	return 0;
